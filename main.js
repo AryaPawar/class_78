20dfe3c4d7b607ba4790da54f2f27d7b5c7b33d1
AryaPawar <72245486+AryaPawar@users.noreply.github.com>
2020-10-30 12:10:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var images = 
+["Family.jpg","Father.jpg","mother.jpg", "child.jpg"];
+var names = ["Family Book","Sunil Pawar", "Yamini Pawar", "Arya Pawar"];
+var i = 0;
+function update()
+{
+i++;
+var numbers_of_family_member_in_array = 3
+if(i > numbers_of_family_member_in_array )
+{
+i = 0;
+}
+var updatedImage = images[i];
+var updatedName  = names[i];
+document.getElementById("family_member_image").src = updatedImage;
+document.getElementById("family_member_name").innerHTML = updatedName;
+ }
